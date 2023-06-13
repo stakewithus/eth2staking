b432ec5cf2033d4bd001f25f3a99ea89ac2ae69f
Poh YH <pohyh@stakewith.us>
2023-06-14 01:13:39 +0800

    Update setup-virtualbox.sh

--- setup-virtualbox.sh
+++ setup-virtualbox.sh
@@ -1,12 +1,20 @@
-sudo apt-get update
-sudo apt install apt-transport-https ca-certificates curl gnupg software-properties-common iptables-persistent -y
-sudo mkdir -m 0755 -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-wget -qO - apt-key add https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+# 1. Updates system and install all necessary items
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt install build-essential -y
+
+# 2. Install docker
+sudo apt-get remove docker docker-engine docker.io containerd runc -y
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo \
-"deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-"$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin -y
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
